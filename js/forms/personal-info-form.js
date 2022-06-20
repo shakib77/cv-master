b9682ab10531a14153b172cfc4004a93aa35a0ce
shakib77 <shakibshaon77@gmail.com>
2022-06-20 10:28:43 +0600

    added image base64 format and modify bod format

--- js/forms/personal-info-form.js
+++ js/forms/personal-info-form.js
@@ -1,3 +1,5 @@
+let personalImage = ''
+
 fetch('https://xosstech.com/cvm/api/public/api/infos', {
     method: "POST", headers: {
         "Content-Type": "application/json", Authorization: bearer
@@ -13,6 +15,17 @@ fetch('https://xosstech.com/cvm/api/public/api/infos', {
             let personalInfoLength = jsonRes.data.length;
             let personalInfoData = jsonRes.data[personalInfoLength - 1];
 
+            personalImage = personalInfoData.image
+
+
+            /*console.log('dbod->',personalInfoData.dob);
+
+            function padTo2Digits(num) {
+                return num.toString().padStart(2, '0');
+            }
+
+            console.log('logdd->', padTo2Digits(personalInfoData.dob.slice(0, 2)));*/
+
             document.getElementById('info_id').value = personalInfoData.id;
             document.getElementById('name').value = personalInfoData.name;
             document.getElementById('email').value = personalInfoData.email;
@@ -24,7 +37,7 @@ fetch('https://xosstech.com/cvm/api/public/api/infos', {
             document.getElementById('religion').value = personalInfoData.religion;
             document.getElementById('nationality').value = personalInfoData.nationality;
             document.getElementById('gender').value = personalInfoData.gender;
-            document.getElementById('dob').value = personalInfoData.dob.split("-").reverse().join("-0");
+            document.getElementById('dob').value = personalInfoData.dob.split("-").reverse().join("-");
             document.getElementById('father_name').value = personalInfoData.father_name;
             document.getElementById('mother_name').value = personalInfoData.mother_name;
             document.getElementById('profile_summary').value = personalInfoData.profile_summary;
@@ -34,10 +47,31 @@ fetch('https://xosstech.com/cvm/api/public/api/infos', {
 
 }).catch((err) => console.log('error=>', err))
 
+function getBase64(file) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = error => reject(error);
+    });
+}
+
 const savePersonalInfo = () => {
     let id = null;
     if (document.getElementById('info_id').value) {
         id = document.getElementById('info_id').value;
+    }
+
+    let file = document.getElementById("image").files[0];
+    let imageFile = '';
+    if (file) {
+        getBase64(file).then(
+            data => {
+                imageFile = data
+            }
+        );
+    } else {
+        imageFile = personalImage;
     }
 
     const formData = {
@@ -51,62 +85,67 @@ const savePersonalInfo = () => {
         religion: document.getElementById("religion").value,
         nationality: document.getElementById("nationality").value,
         gender: document.getElementById("gender").value,
-        dob: document.getElementById("dob").value,
+        dob: document.getElementById("dob").value.split("-").reverse().join("-"),
         profile_summary: document.getElementById("profile_summary").value,
         father_name: document.getElementById("father_name").value,
         mother_name: document.getElementById("mother_name").value,
-        image: document.getElementById("image").files[0],
     };
 
-    // fileInput.files[0], "file")
-    /*const formData = new FormData();
-    formData.append("name", document.getElementById("name").value);
-    formData.append("image", document.getElementById("image").files[0]);
-    formData.append("mobile", document.getElementById("mobile").value);
-    formData.append("email", document.getElementById("email").value);
-    formData.append("present_address", document.getElementById("present_address").value);
-    formData.append("permanent_address", document.getElementById("permanent_address").value);
-    formData.append("job_title", document.getElementById("job_title").value);
-    formData.append("marital_status", document.getElementById("marital_status").value);
-    formData.append("religion", document.getElementById("religion").value);
-    formData.append("nationality", document.getElementById("nationality").value);
-    formData.append("gender", document.getElementById("gender").value);
-    formData.append("dob", document.getElementById("dob").value);
-    formData.append("profile_summary", document.getElementById("profile_summary").value);
-    formData.append("father_name", document.getElementById("father_name").value);
-    formData.append("mother_name", document.getElementById("mother_name").value);*/
+    setTimeout(apiCall, 2000);
 
-    // console.log('sdfd->', document.getElementById("dob").value.split("-").reverse().join("-"));
+    function apiCall () {
+        formData.image = imageFile;
 
-    fetch(id ? `https://xosstech.com/cvm/api/public/api/info/update/${id}` : 'https://xosstech.com/cvm/api/public/api/info', {
-        method: "POST",
-        mode: "cors",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: bearer,
-        },
-        body: JSON.stringify(formData),
-    }).then((res) => {
-        console.log('res=>', res);
-        if (!res.ok) {
-            throw Error("Could not fetch data for that resource");
-        } else {
-            return res.json();
-        }
-    })
-        .then((jsonRes) => {
-            console.log('jsonRes =>', jsonRes);
-            if (!jsonRes.success) {
-                console.log('!jsonRes.success->', jsonRes);
-                // window.location.href = "login.html";
+        // fileInput.files[0], "file")
+        /*const formData = new FormData();
+        formData.append("name", document.getElementById("name").value);
+        formData.append("image", document.getElementById("image").files[0]);
+        formData.append("mobile", document.getElementById("mobile").value);
+        formData.append("email", document.getElementById("email").value);
+        formData.append("present_address", document.getElementById("present_address").value);
+        formData.append("permanent_address", document.getElementById("permanent_address").value);
+        formData.append("job_title", document.getElementById("job_title").value);
+        formData.append("marital_status", document.getElementById("marital_status").value);
+        formData.append("religion", document.getElementById("religion").value);
+        formData.append("nationality", document.getElementById("nationality").value);
+        formData.append("gender", document.getElementById("gender").value);
+        formData.append("dob", document.getElementById("dob").value);
+        formData.append("profile_summary", document.getElementById("profile_summary").value);
+        formData.append("father_name", document.getElementById("father_name").value);
+        formData.append("mother_name", document.getElementById("mother_name").value);*/
+
+        // console.log('sdfd->', document.getElementById("dob").value.split("-").reverse().join("-"));
+
+        fetch(id ? `https://xosstech.com/cvm/api/public/api/info/update/${id}` : 'https://xosstech.com/cvm/api/public/api/info', {
+            method: "POST",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: bearer,
+            },
+            body: JSON.stringify(formData),
+        }).then((res) => {
+            console.log('res=>', res);
+            if (!res.ok) {
+                throw Error("Could not fetch data for that resource");
             } else {
-                console.log('jsonRes.success->', jsonRes);
-                alert(id ? 'Data has been successfully updated' : 'Data has been successfully submitted');
-                // document.getElementById("personal_info_from").reset();
+                return res.json();
             }
         })
-        .catch((err) => {
-            console.log('error->', err);
-            // window.location.href = "/login.html";
-        });
+            .then((jsonRes) => {
+                console.log('jsonRes =>', jsonRes);
+                if (!jsonRes.success) {
+                    console.log('!jsonRes.success->', jsonRes);
+                    // window.location.href = "login.html";
+                } else {
+                    console.log('jsonRes.success->', jsonRes);
+                    alert(id ? 'Data has been successfully updated' : 'Data has been successfully submitted');
+                    // document.getElementById("personal_info_from").reset();
+                }
+            })
+            .catch((err) => {
+                console.log('error->', err);
+                // window.location.href = "/login.html";
+            });
+    }
 }
